262196bca8ca08ff15aab65c47953e25d8dcac09
davi <davi@gmail.com>
2023-12-26 09:30:27 -0300

    TADS implementado

--- TADs/code/app.c
+++ TADs/code/app.c
@@ -4,6 +4,13 @@
 int main(){
 
     FloatVector *vec = create(10);
+    print(vec);
+
+    append(vec,56.4);
+    append(vec, 454.6);
+    append(vec, 565.4);
+
+    print(vec);
 
     destroy(&vec);
 
